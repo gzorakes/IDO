ec1f07c28f0b99b7fb07f3ca2bf0af51fd00073a
gzorakes <gzorakes@gmail.com>
2025-03-06 23:54:19 +0200

    [Feature] Add WelcomeView UI

--- IDO/Core/Welcome/WelcomeView.swift
+++ IDO/Core/Welcome/WelcomeView.swift
@@ -8,20 +8,72 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    @State var imageName: String = Constants.randomImage
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome!")
-                    .frame(maxHeight: .infinity)
+            VStack(spacing: 8.0) {
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
                 
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get Started")
-                        .callToActionButton()
-                }
+                titleSection
+                    .padding(.top, 24)
+                    .fontDesign(.rounded)
+                
+                ctaButtons
+                    .padding(16)
+                
+                policyLinks
             }
-            .padding(16)
+        }
+    }
+    
+    private var titleSection: some View {
+        VStack {
+            Text("Yes I Do! 💍")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.accent)
+            
+            Text("by George Zorakis")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            
+        }
+    }
+    
+    private var ctaButtons: some View {
+        VStack {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+            }
+            
+            Text("Already have an account? Sign in!")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .tappableBackground()
+                .onTapGesture {
+                    
+                }
+        }
+    }
+    
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsOfServiceUrl)!) {
+                Text("Terms of Service")
+            }
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+            Link(destination: URL(string: Constants.privacyPolicyUrl)!) {
+                Text("Privacy Policy")
+            }
         }
     }
 }
---
+++ IDO/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  IDO
+//
+//  Created by Γιωργος Ζωρακης on 6/3/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyUrl = "https://www.apple.com"
+    static let termsOfServiceUrl = "https://www.apple.com"
+}
