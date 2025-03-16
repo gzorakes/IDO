82d2502c81969dd0567ab35e31431cc3b0bc4992
gzorakes <gzorakes@gmail.com>
2025-03-17 00:00:56 +0200

    [Feature] Chat ScrollView

--- IDO/Core/ChatView/ChatView.swift
+++ IDO/Core/ChatView/ChatView.swift
@@ -11,28 +11,80 @@ struct ChatView: View {
     
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
     @State private var currentUser: UserModel? = .mock
+    @State private var textFieldText: String = ""
     
     var body: some View {
         VStack(spacing: 0) {
-            ScrollView {
-                LazyVStack(spacing: 24) {
-                    ForEach(chatMessages) { message in
-                        let isCurrentUser = message.authorId == currentUser?.userId
-                        ChatBubbleViewBuilder(
-                            message: message,
-                            isCurrentUser: isCurrentUser
-                        )
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding(8)
-            }
-            
-            Rectangle()
-                .frame(height: 50)
+            scrollViewSection
+            textFieldSection
         }
         .navigationTitle(currentUser?.name ?? "Chat")
         .toolbarTitleDisplayMode(.inline)
+    }
+    
+    private var scrollViewSection: some View {
+        ScrollView {
+            LazyVStack(spacing: 16) {
+                ForEach(chatMessages) { message in
+                    let isCurrentUser = message.authorId == currentUser?.userId
+                    ChatBubbleViewBuilder(
+                        message: message,
+                        isCurrentUser: isCurrentUser
+                    )
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(8)
+            .rotationEffect(.degrees(180))
+        }
+        .rotationEffect(.degrees(180))
+        .animation(.default, value: chatMessages.count)
+    }
+    
+    private var textFieldSection: some View {
+        TextField("Say something...", text: $textFieldText)
+            .keyboardType(.alphabet)
+            .autocorrectionDisabled()
+            .padding(12)
+            .padding(.trailing, 50)
+            .overlay(alignment: .trailing) {
+                Image(systemName: "arrow.up.circle.fill")
+                    .font(.system(size: 32))
+                    .padding(.trailing, 4)
+                    .foregroundStyle(.accent)
+                    .anyButton(.plain) {
+                        onSendMessagePressed()
+                    }
+            }
+            .background(
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color(uiColor: .systemBackground))
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                }
+            )
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(Color(uiColor: .secondarySystemBackground))
+    }
+    
+    private func onSendMessagePressed() {
+        guard let currentUser else { return }
+        let content = textFieldText
+        
+        let message = ChatMessageModel(
+            id: UUID().uuidString,
+            chatId: UUID().uuidString,
+            authorId: currentUser.userId,
+            content: content,
+            dateCreated: .now
+        )
+        
+        chatMessages.append(message)
+        
+        textFieldText = ""
     }
 }
 
