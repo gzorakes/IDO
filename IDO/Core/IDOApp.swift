04642deb841f257bad7c9c3a06b89c46c9f8c13f
gzorakes <gzorakes@gmail.com>
2025-03-17 22:54:53 +0200

    [Feature] Add Firebase

--- IDO/Core/IDOApp.swift
+++ IDO/Core/IDOApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct IDOApp: App {
     var body: some Scene {
+        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+        
         WindowGroup {
             AppView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
