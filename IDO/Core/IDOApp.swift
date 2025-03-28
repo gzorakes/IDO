f0113858d9b7f4289313db9c34faf2b3fdb8e6cc
gzorakes <gzorakes@gmail.com>
2025-03-28 16:23:17 +0200

    [Feature] Multiple Firebase info.plist

--- IDO/Core/IDOApp.swift
+++ IDO/Core/IDOApp.swift
@@ -45,22 +45,42 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     var dependencies: Dependencies!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
+        
+        let config: BuildConfiguration
         
         #if MOCK
-        dependencies = Dependencies(config: .mock(isSignedIn: true))
+        config = .mock(isSignedIn: true)
         #elseif DEV
-        dependencies = Dependencies(config: .dev)
+        config = .dev
         #else
-        dependencies = Dependencies(config: .prod)
+        config = .prod
         #endif
 
+        config.configure()
+        dependencies = Dependencies(config: config)
         return true
     }
 }
 
 enum BuildConfiguration {
     case mock(isSignedIn: Bool), dev, prod
+    
+    func configure() {
+        switch self {
+        case .mock(let isSignedIn):
+            // Mock build does not run Firebase
+            break
+        case .dev:
+            let plist = Bundle.main.path(forResource: "GoogleService-Info-Dev", ofType: "plist")!
+            let options = FirebaseOptions(contentsOfFile: plist)!
+            FirebaseApp.configure(options: options)
+        case .prod:
+            let plist = Bundle.main.path(forResource: "GoogleService-Info-Prod", ofType: "plist")!
+            let options = FirebaseOptions(contentsOfFile: plist)!
+            FirebaseApp.configure(options: options)
+
+        }
+    }
 }
 
 
